ae509a1692abcf946b12fc118faad832ed46c56c
Ashish-Rautela <chessashishrautela@gmail.com>
2025-03-21 22:28:12 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Add your image link here
-document.getElementById("gameImage").src = "./image_game.jpg";
-
-document.getElementById("gameBox").addEventListener("click", function() {
-    window.location.href = "./games/games.html"; 
-});
