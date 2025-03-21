c79201de0987d60371b46cfd96cc1bf56f2cb67e
Ashish-Rautela <chessashishrautela@gmail.com>
2025-03-21 20:33:57 +0530

    Add files via upload

---
+++ Ashish-Rautela/script.js
@@ -0,0 +1,6 @@
+// Add your image link here
+document.getElementById("gameImage").src = "./image_game.jpg";
+
+document.getElementById("gameBox").addEventListener("click", function() {
+    window.location.href = "./games/games.html"; 
+});
