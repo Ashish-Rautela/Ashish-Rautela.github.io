672781551472ae382fb9e1518af51d2d600148ae
Ashish-Rautela <chessashishrautela@gmail.com>
2025-03-22 17:51:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ document.getElementById("gameImage").src = "./image_game.jpg";
 document.getElementById("gameBox").addEventListener("click", function() {
     window.location.href = "./games/games.html"; 
 });
+document.getElementById("coming-soon-box").addEventListener("click", function() {
+    window.location.href = "./coming soon/index.html"; 
+});
