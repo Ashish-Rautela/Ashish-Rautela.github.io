0903104ae967a008ffab5d20648272bd12708d20
Ashish-Rautela <chessashishrautela@gmail.com>
2025-04-06 18:37:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-
 document.getElementById("gameImage").src = "./image_game.jpg";
+document.getElementById("qrImage").src = "./qr.png";
 
 document.getElementById("gameBox").addEventListener("click", function() {
     window.location.href = "./games/games.html"; 
+});
+document.getElementById("qr-code-box").addEventListener("click", function() {
+    window.location.href = "./QR_CODE_GEN/index.html"; 
 });
 document.getElementById("coming-soon-box").addEventListener("click", function() {
     window.location.href = "./coming soon/index.html"; 
